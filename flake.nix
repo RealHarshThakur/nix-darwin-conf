e06e38633136bf3108ec0f97c42b4c4362cda5d6
harsh <harsh@harshs-Mac-mini.local>
2024-08-07 11:34:52 +0530

    add gcloud with auth plugin
    Signed-off-by: harsh <harsh@harshs-Mac-mini.local>

--- flake.nix
+++ flake.nix
@@ -32,6 +32,8 @@
 	  pkgs.lima
           pkgs.skopeo
           pkgs.goreleaser
+          (pkgs.google-cloud-sdk.withExtraComponents [ pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin])
+
 ];
 
 
